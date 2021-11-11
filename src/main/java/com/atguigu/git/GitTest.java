b7ebb144a0885bc9031f66fddb496dca3aa5b7d7
Erik <Erik@test.com>
2021-11-11 21:13:48 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author Erik_Wong
+ * @version 1.0
+ * @date 2021/11/11 21:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
